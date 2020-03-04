3d1a456a883911737c9edf521a0dba1b590118c2
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-04 18:46:07 +0100

    add factory constructor

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 var Test = Object.getPrototypeOf(app).Test = new Component("test");
-Test.debug = true;
+// Test.debug = true;
 Test.createdAt      = "2.0.0";
 Test.lastUpdate     = "2.0.0";
 Test.version        = "1";
-// Test.loadingMsg     = "This message will display in the console when component will be loaded.";
+// Test.factoryExclude = true;
+Test.loadingMsg     = "This message will display in the console when component will be loaded.";
 
 // Test.prototype.onCreate = function(){
   // do thing after element's creation
