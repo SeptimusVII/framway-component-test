b2a7e8ae6c05e758a746382b4e4e6f41a8898a14
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-10 19:18:55 +0100

    remove loading msg

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ Test.createdAt      = "2.0.0";
 Test.lastUpdate     = "2.0.0";
 Test.version        = "1";
 // Test.factoryExclude = true;
-Test.loadingMsg     = "This message will display in the console when component will be loaded.";
+// Test.loadingMsg     = "This message will display in the console when component will be loaded.";
 
 // Test.prototype.onCreate = function(){
   // do thing after element's creation
