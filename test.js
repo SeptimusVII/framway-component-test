ab268fb642f9847fc0b4150a1048b6893910a5f6
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-04-15 12:05:46 +0200

    component is now exported as a separate module, that properly requires app to work

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-var Test = Object.getPrototypeOf(app).Test = new Component("test");
-// Test.debug = true;
-Test.createdAt      = "2.0.0";
-Test.lastUpdate     = "2.0.0";
-Test.version        = "1";
-// Test.factoryExclude = true;
-// Test.loadingMsg     = "This message will display in the console when component will be loaded.";
+module.exports = function(app){
+	var Test = Object.getPrototypeOf(app).Test = new app.Component("test");
+	// Test.debug = true;
+	Test.createdAt      = "2.0.0";
+	Test.lastUpdate     = "2.0.0";
+	Test.version        = "1";
+	// Test.factoryExclude = true;
+	// Test.loadingMsg     = "This message will display in the console when component will be loaded.";
 
-// Test.prototype.onCreate = function(){
-  // do thing after element's creation
-// }
+	// Test.prototype.onCreate = function(){
+	  // do thing after element's creation
+	// }
+	
+	return Test;
+}
